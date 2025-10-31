2ff2adbaa5b6ba28adae0e1e28db6fb53bb4a2b8
rohithredddy <141226920+rohithredddy@users.noreply.github.com>
2025-11-01 00:27:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,30 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import RoheeWidget from "./components/RoheeWidget";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+
+        {/* 💬 Rohee Chatbot Widget */}
+        <RoheeWidget apiBaseUrl="https://rohee-backend.onrender.com" />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
 
 export default App;
